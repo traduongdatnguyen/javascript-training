b64f86567e39de88f47ea80591e12a3009322e87
traduongdatnguyen <tddnguyen.20it8@vku.udn.vn>
2022-03-24 23:03:53 +0700

    Day 2

---
+++ Week 1/Day 2_Chapter3/example/BeanCounting.js
@@ -0,0 +1,15 @@
+function countChar(string, ch) {
+    let counted = 0;
+    for (let i = 0; i < string.length; i++) {
+      if (string[i] == ch) {
+        counted += 1;
+      }
+    }
+    return counted;
+  }
+  
+  function countBs(string) {
+    return countChar(string, "B");
+  }
+  
+  console.log(countBs("BBC"));
---
+++ Week 1/Day 2_Chapter3/example/minimun.js
@@ -0,0 +1,6 @@
+function minimun(a,b) {
+    if(a > b){ return b}else{
+        return a;
+    }
+}
+console.log(minimun(2,5));
